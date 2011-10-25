9a23fc48f6e21e72c6b4d722bc5ba26895da51d4
Edward_Acer <Edward_Acer@Edward_Acer-PC.homenet.telecomitalia.it>
2011-10-26 00:20:04 +0200

    EliminaFotoVecchie test Compilazione

--- Digiphoto.Lumen.Core.VsTest/EliminaFotoVecchieSrvImplTest.cs
+++ Digiphoto.Lumen.Core.VsTest/EliminaFotoVecchieSrvImplTest.cs
@@ -4,12 +4,13 @@ using System;
 using System.Collections.Generic;
 using Digiphoto.Lumen.Applicazione;
 using Digiphoto.Lumen.Model;
-using Digiphoto.Lumen.src.Database;
 using Digiphoto.Lumen.Database;
 using System.Linq;
 using System.Text;
 using System.IO;
 using System.Transactions;
+using System.Diagnostics;
+using Digiphoto.Lumen.Properties;
 
 namespace Digiphoto.Lumen.Core.VsTest
 {
@@ -25,33 +26,75 @@ namespace Digiphoto.Lumen.Core.VsTest
 
         private EliminaFotoVecchieSrvImpl _impl = new EliminaFotoVecchieSrvImpl();
 
-        private Fotografo _fotografo;
+		Fotografo _mario = null;
+
+		private int giorni = 0;
 
         [TestInitialize()]
         public void initTest()
         {
-            LumenEntities objContext = UnitOfWorkScope.CurrentObjectContext;
-            _fotografo = objContext.Fotografi.Single<Fotografo>(ff => ff.id == "ROSSIMARIO");
-            LumenApplication app = LumenApplication.Instance;
-            app.avvia();
+			Console.WriteLine("INIZIO");
+			LumenApplication app = LumenApplication.Instance;
+			app.avvia();
+			giorni = Settings.Default.giorniDeleteFoto;
+			Console.WriteLine("GIORNI " +giorni);
+
+			using (LumenEntities dbContext = new LumenEntities()) {
+				_mario = Utilita.ottieniFotografoMario(dbContext);
+			}
+            
             ParamEliminaFotoVecchieSrv paramEliminaFotoVecchie = new ParamEliminaFotoVecchieSrv();
-            paramEliminaFotoVecchie.fotografo = _fotografo;
+			paramEliminaFotoVecchie.fotografo = _mario;
             paramEliminaFotoVecchie.dataIntervallo = DateTime.Now;
+			_impl = new EliminaFotoVecchieSrvImpl();
+			//_impl.start();
             _impl.init(paramEliminaFotoVecchie);
-            _impl = new EliminaFotoVecchieSrvImpl();
-            _impl.start();
+           
         }
 
         [TestMethod()]
         public void diChiSonoQuesteFoto()
         {
-            Assert.IsTrue(_impl.diChiSonoQuesteFoto().Equals("ROSSIMARIO"));
+			Assert.IsTrue(_impl.diChiSonoQuesteFoto().id.Equals("ROSSIMARIO"));
         }
 
          [TestMethod()]
         public void getListaCartelleDaEliminare()
         {
-            Assert.IsTrue(_impl.getListaCartelleDaEliminare()[0].Equals(@"C:\ProgramData\digiPHOTO\Lumen\Foto\2011-10-21\ROSSIMARIO"));
+			if (_impl.getListaCartelleDaEliminare().Count()==0)
+			{
+				Assert.IsTrue(true);
+			}
+			else
+			{
+				Assert.IsTrue(_impl.getListaCartelleDaEliminare()[0].Equals(@"C:\ProgramData\digiPHOTO\Lumen\Foto\"+DateTime.Now.AddDays(-giorni).ToString("yyyy-MM-dd")+@"\ROSSIMARIO"));
+			}
         }
+
+		 [TestMethod()]
+		 public void getElimina()
+		 {
+			 if (_impl.getListaCartelleDaEliminare().Count() == 0)
+			 {
+				 Console.WriteLine("Lista Nulla");
+				 Assert.IsTrue(true);
+			 }
+			 else
+			 {
+				 Console.WriteLine("Lista NON Nulla");
+				 IList<String> list = _impl.getListaCartelleDaEliminare();
+				 foreach(String path in list){
+					 Trace.WriteLine(path);
+					_impl.elimina(path);
+				 }
+			 }
+		 }
+
+		 [TestMethod()]
+		 public void testEliminaRecord()
+		 {
+			 _impl.eliminaRecord();
+			 Assert.IsTrue(true);
+		 }
     }
 }
