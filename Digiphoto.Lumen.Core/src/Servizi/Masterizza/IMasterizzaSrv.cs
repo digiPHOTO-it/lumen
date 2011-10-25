1d3dbb13bc230b438ef7c7d252507ced3d68d2d4
Luca Bernardini <54762057+digiPHOTO-it@users.noreply.github.com>
2011-10-25 22:28:59 +0200

    Mancava un accessor per i test Creato nuova enumerazione per le fasi del giorno

---
+++ Digiphoto.Lumen.Core/src/Servizi/Masterizza/IMasterizzaSrv.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Digiphoto.Lumen.Servizi;
+using System.Collections;
+using Digiphoto.Lumen.Model;
+
+namespace Digiphoto.Lumen.Servizi.Masterizza {
+
+	public enum TipoDestinazione {
+		MASTERIZZATORE,
+		CARTELLA
+	}
+
+	/**
+	 * Questo servizio implementa una coda di attesa di foto che devono essere copiate.
+	 * Al momento della copia, occorre creare un carrello che registra l'incasso.
+	 * Attenzione: le foto devono essere elaborate: non possiamo vendere i provini ma quelle buone.	
+	 */
+	public interface IMasterizzaSrv : IServizio, IList<Fotografia> {
+
+		/** Aggiunge l'intero album alla lista */
+		void add( Album album );
+
+		/** L'utente deve scegliere dove copiare le foto.
+		 * Se il tipo è CARTELLA, allora la destinazione è il nome della cartella.
+		 * Se il tipo è MASTERIZZATORE allora la destinazione indica il 
+		 * masterizzatore da usare (potrebbero essercene più di uno).
+		 */
+		void impostaDestinazione( TipoDestinazione tipoDestinazione, String destinazione );
+
+		/** Il prezzo del disco masterizzato, viene sempre contrattato con il cliente */
+		Carrello confermaVendita( Decimal prezzoForfettario );
+	}
+}
