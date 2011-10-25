f21834b93b18c8bf8393b24e6b4974a1e42dded4
Luca Bernardini <54762057+digiPHOTO-it@users.noreply.github.com>
2011-10-25 22:37:03 +0200

    mancavano due file anche se vuoti

---
+++ Digiphoto.Lumen.Core.VsTest/src/Util/QueriesVarieTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Digiphoto.Lumen.Applicazione;
+using Digiphoto.Lumen.Database;
+using Digiphoto.Lumen.Model;
+
+namespace Digiphoto.Lumen.Core.VsTest.Util {
+
+	[TestClass]
+
+	public class QueriesVarieTest {
+	
+		[TestMethod]
+		public void TestQuery1() {
+
+			LumenApplication.Instance.avvia();
+
+			using( LumenEntities dbContext = new LumenEntities() ) {
+				
+				// modalità di query di tipo LINQ 
+				var chiavi =
+					from f
+					in dbContext.Fotografie
+					where 1==1
+					select f.id;
+
+
+				// -----
+
+				// carico una lista di ID per provare una query con il Contains
+				Guid [] chiaviArray = chiavi.ToArray();
+
+				var fotos = from f2
+								in dbContext.Fotografie
+							where chiaviArray.Contains( f2.id )
+							select f2;
+
+				IEnumerator<Fotografia> itera = fotos.GetEnumerator();
+				itera.MoveNext();
+				var campione = itera.Current;
+
+				// -- provo a fare una query con tutta l'entità
+
+				try {
+					var foto3 = from f3
+								in dbContext.Fotografie
+								where f3.Equals( campione )
+								select f3;
+					Assert.Fail();
+				} catch( Exception ee ) {
+					// deve dare errore perchè nelle query si possono usare solo tipi primitivi per l'uguaglianza
+					//    :-((   bleah!!!
+				}
+			}
+
+		}
+	}
+}
---
+++ Digiphoto.Lumen.Core/src/Servizi/Ricerca/IRicercatoreSrv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Digiphoto.Lumen.Model;
+using Digiphoto.Lumen.Core;
+
+namespace Digiphoto.Lumen.Servizi.Ricerca {
+
+	public class ParamRicerca {
+
+		public Evento [] eventi { get; set; }
+		public string Didascalia { get; set; }
+		public Fotografo [] fotografo { get; set; }
+		public FaseDelGiorno [] fasiDelGiorno { get; set; }
+		public int [] numeriFotogrammi;
+
+		DateTime? giornataIniz { get; set; }
+		DateTime? giornataFine { get; set; }
+	}
+
+	interface IRicercatoreSrv {
+	}
+}
