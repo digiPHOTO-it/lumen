45dbcfc6af9002322552c1f6dd7c37443de7521f
Luca Bernardini <lucasimone.bernardini@gmail.com>
2011-10-26 14:33:04 +0200

    Abbozzato il servizio di ricerca fotografie. Scritto un test che intanto qualcosa legge.

--- Digiphoto.Lumen.Core/src/Servizi/Ricerca/IRicercatoreSrv.cs
+++ Digiphoto.Lumen.Core/src/Servizi/Ricerca/IRicercatoreSrv.cs
@@ -7,18 +7,28 @@ using Digiphoto.Lumen.Core;
 
 namespace Digiphoto.Lumen.Servizi.Ricerca {
 
-	public class ParamRicerca {
+	public class ParamRicercaFoto {
 
 		public Evento [] eventi { get; set; }
-		public string Didascalia { get; set; }
+		public string didascalia { get; set; }
 		public Fotografo [] fotografo { get; set; }
 		public FaseDelGiorno [] fasiDelGiorno { get; set; }
-		public int [] numeriFotogrammi;
+		public int [] numeriFotogrammi {get; set; }
 
-		DateTime? giornataIniz { get; set; }
-		DateTime? giornataFine { get; set; }
+		public DateTime? giornataIniz { get; set; }
+		public DateTime? giornataFine { get; set; }
+
+		/** Numero di record da saltare nel risultato. Se NULL allora niente */
+		public long? paginazioneSkip { get; set; }
+
+		/** Numero di record di ampiezza della paginazione. Se NULL allora ninente */
+		public int? paginazioneLimit { get; set; }
+
 	}
 
 	interface IRicercatoreSrv {
+
+		IList<Fotografia> cerca( ParamRicercaFoto param );
+
 	}
 }
