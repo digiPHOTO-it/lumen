91eb89f791de4798dcf622b2a92e9ce13f94dd0a
Edward_Acer <Edward_Acer@Edward_Acer-PC>
2012-01-31 00:27:19 +0100

    Aggiunte classe per UserControl SelezionaFormatiCarta

---
+++ Digiphoto.Lumen.Core/src/Servizi/EntityRepository/FormatiCartaRepositorySrvImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Digiphoto.Lumen.Model;
+using Digiphoto.Lumen.Core.Database;
+using Digiphoto.Lumen.Servizi.EntityRepository;
+
+namespace Digiphoto.Lumen.Servizi.EntityRepository
+{
+    public class FormatiCartaRepositorySrvImpl : EntityRepositorySrvImpl<FormatoCarta>
+    {
+        public FormatiCartaRepositorySrvImpl() : base() {
+		}
+
+		public override FormatoCarta getById( object oid ) {
+			Guid id = (Guid)oid;
+			return UnitOfWorkScope.CurrentObjectContext.FormatiCarta.SingleOrDefault( f => f.id == id );
+		}
+    }
+}
