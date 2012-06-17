08d65e8dfbdc5c0c0ae34ce2def0af41025818d4
Edward84 <Edward84@localhost>
2012-06-17 12:13:11 +0200

    Implementato sistema di log su console esterna; la console rimane sempre in primo piano.

---
+++ Digiphoto.Lumen.UI/Logging/LoggingShowWindowl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Digiphoto.Lumen.UI.Logging
+{
+	/// <summary>
+	/// Interaction logic for LoggingShowWindowsxaml.xaml
+	/// </summary>
+	public partial class LoggingShowWindows : Window
+	{
+		public LoggingShowWindows()
+		{
+			InitializeComponent();
+		}
+	}
+}
