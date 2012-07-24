af5bc23f5aa2856866c91a06fc3497de35c4d4f0
Luca Bernardini <lucasimone.bernardini@gmail.com>
2012-07-24 13:15:34 +0200

    ops

---
+++ Digiphoto.Lumen.Core/src/Config/ConfigurazioneMancanteException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Digiphoto.Lumen.Config {
+
+	public class ConfigurazioneMancanteException : ConfigurationErrorsException {
+
+		public ConfigurazioneMancanteException() {
+		}
+
+		public ConfigurazioneMancanteException( string sMessage, Exception innerException )
+			: base( sMessage, innerException ) {
+		}
+
+		public ConfigurazioneMancanteException (string sMessage)
+			: base(sMessage) { 
+		}
+	}
+}
