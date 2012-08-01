1b5302e1853130d44d7a2245921fb171a64432e0
Edward84 <Edward84@localhost>
2012-08-01 15:34:13 +0200

    Issue #102 "Carrelli Salvati GUI ricerca"
    Inserito numero progressivo

---
+++ Digiphoto.Lumen.UI/Converters/ListItemIndexConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using Digiphoto.Lumen.Imaging;
+using Digiphoto.Lumen.Imaging.Wic;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Digiphoto.Lumen.UI.Converters {
+
+	/// <summary>
+	/// Questo convertitore mi serve per convertire una immagine intesa come interfaccia IImmagine
+	/// in un componente renderizzabile in un controllo Image.
+	/// </summary>
+	public class ListItemIndexConverter : IValueConverter
+	{
+
+		public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+			ListBoxItem item = value as ListBoxItem;
+            ListBox view = ItemsControl.ItemsControlFromItemContainer(item) as ListBox;
+			int index = view.ItemContainerGenerator.IndexFromContainer(item);
+			return index+1;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) {
+			throw new NotImplementedException();
+		}
+	}
+}
