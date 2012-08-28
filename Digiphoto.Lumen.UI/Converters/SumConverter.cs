8839163fb584892f535ba4a36b1317b05dddb310
Edward84 <Edward84@localhost>
2012-08-28 09:55:49 +0200

    Migliorato zoom foto nelle righe del carrello

---
+++ Digiphoto.Lumen.UI/Converters/SumConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Digiphoto.Lumen.UI.Converters {
+
+	public class SumConverter : IValueConverter {
+
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			return System.Convert.ToDouble( value, culture ) + System.Convert.ToDouble( parameter, culture );
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter,	CultureInfo culture ) {
+			return System.Convert.ToDouble(value, culture) + System.Convert.ToDouble(parameter, culture);
+		}
+	}
+}
