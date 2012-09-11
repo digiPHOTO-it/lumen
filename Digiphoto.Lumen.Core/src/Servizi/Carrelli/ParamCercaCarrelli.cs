8f117a143edbb9222df30de18495843a8e55d879
Edward84 <Edward84@localhost>
2012-09-11 15:09:53 +0200

    Corretto errore warning

--- Digiphoto.Lumen.Core/src/Servizi/Carrelli/ParamCercaCarrelli.cs
+++ Digiphoto.Lumen.Core/src/Servizi/Carrelli/ParamCercaCarrelli.cs
@@ -5,19 +5,11 @@ using System.Text;
 using Digiphoto.Lumen.Util;
 
 namespace Digiphoto.Lumen.Servizi.Carrelli {
-	
-
 
 	public class ParamCercaCarrelli : ParamCerca {
 
 		/** Filtro sul timestamp di registrazione */
 		public DateTime? dateTimeRegistrazioneIniz { get; set; }
 		public DateTime? dateTimeRegistrazioneFine { get; set; }
-
-		/**   0=tutti  ;  1=solo temporanei  ; 2=solo definitivi */
-		short stato;
-
-		/** Eventuale like sulla descrizione (tutta maiuscola) */
-		string intestazione;
 	}
 }
