a0bc80eef49adc38684cc943c1b3df3dec4f38b4
Luca Bernardini <lucasimone.bernardini@gmail.com>
2012-09-19 09:51:24 +0200

    Issue #107  Gestione maschere : la foto copre i pulsanti di azione. Ho centrato la cornice nel centro dello schermo. Per fare ci? ho sostituito il Canvas che contiene la cornice, con un Grid. Infatti il Grid si preoccupa da solo automaticamente di centrare il contenuto. Poi ho modificato il metodo Geometrie::proporziona() perch? gli mancavano due informazioni: il Left ed il Top (che prima essendo tutto a sinistra erano = 0).

--- Digiphoto.Lumen.Core/src/Imaging/Geometrie.cs
+++ Digiphoto.Lumen.Core/src/Imaging/Geometrie.cs
@@ -11,24 +11,29 @@ namespace Digiphoto.Lumen.Imaging {
 		/// <summary>
 		///   
 		///  a : b = x : c
-		///   Questa mi serve nel crop.
-		///   Io ho un rettangolo (A) di crop riferita ad una immagine che vedo nello schermo (B)
-		///   voglio applicare il crop ad un'altra immagine (C).
+		///   Questa mi serve nel crop e nelle maschere.
+		///   Io ho un rettangolo (A) di crop riferita ad una immagine ridotta che vedo nello schermo (B)
+		///   voglio applicare il crop ad un'altra immagine (C) di grandezza naturale.
 		///   Che area devo croppare ???? (x)
 		///   
+		///   <param name="a">Rettangolo di crop</param>
+		///   <param name="b">Rettangolo con la foto ridotta per stare nello schermo</param>
+		///   <param name="c">Dimensione della foto originale a grandezza naturale da croppare</param>
+		/// 
 		///    A : B = x : C
 		///    x = A * C / B
 		/// 
 		/// </summary>
-		public static Rect proporziona( Rect a, Size b, Size c ) {
+		public static Rect proporziona( Rect a, Rect b, Size c ) {
 
 			Rect ris = new Rect();
-			ris.X =      ((a.X * c.Width) / b.Width);
-			ris.Y =      ((a.Y * c.Height) / b.Height);
-			ris.Width =  ((a.Width * c.Width) / b.Width);
+			ris.X = (((a.X -b.X) * c.Width) / b.Width);
+			ris.Y = (((a.Y - b.Y) * c.Height) / b.Height);
+			ris.Width = ((a.Width * c.Width) / b.Width);
 			ris.Height = ((a.Height * c.Height) / b.Height);
 			return ris;
 		}
+
 
 		public static Int32Rect proporziona( Int32Rect a, Int32Size b, Int32Size c ) {
 
