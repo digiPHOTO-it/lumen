3278a9991cf31b3f7c62b65161838f28d1655a0e
Edward84 <Edward84@localhost>
2012-11-13 23:45:26 +0100

    Issue #44 "Crud entit? Eventi"

---
+++ Digiphoto.Lumen.UI/DataEntry/DEEvento/DataEntryEventoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using Digiphoto.Lumen.Model;
+using System.Linq.Expressions;
+using Digiphoto.Lumen.Applicazione;
+using Digiphoto.Lumen.Servizi.Vendere;
+
+namespace Digiphoto.Lumen.UI.DataEntry.DEEvento {
+
+	public class DataEntryEventoViewModel : DataEntryViewModel<Evento> {
+
+		protected override void passoPreparaAddNew( Evento evento ) {
+			evento.id = Guid.NewGuid();
+			evento.attivo = true;
+		}
+
+		protected override void passoPrimaDiSalvare( Evento fotografo ) {
+			collectionView.Refresh();
+		}
+
+		protected override object passoCaricaDati() {
+			IQueryable<Evento> q = entityRepositorySrv.Query();
+			return q.OrderByDescending( gg => gg.id );
+		}
+
+		protected override void passoPreparaEdit( Evento evento ) {
+			collectionView.Refresh();
+		}
+	}
+}
