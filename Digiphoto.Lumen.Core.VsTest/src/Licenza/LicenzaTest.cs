39f86b293b44175db959da7c8d3e95934d8155ce
Luca Bernardini <54762057+digiPHOTO-it@users.noreply.github.com>
2013-03-14 19:02:37 +0100

    Issue #177 Gestione licenze. Prima stesura. Nella gestione della configurazione, creato un nuovo componente di nome LicenseEditor. Quando si salva la configurazione, viene memorizzata la chiave nel registry.

---
+++ Digiphoto.Lumen.Core.VsTest/src/Licenza/LicenzaTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Digiphoto.Lumen.Core.VsTest.Licenza {
+
+	[TestClass]
+
+
+	public class LicenzaTest {
+
+
+		[TestMethod]
+		public void TestLicenza1() {
+
+			SKGL.Validate ValidateAKey = new SKGL.Validate();// create an object
+			ValidateAKey.secretPhase = "My$ecretPa$$W0rd"; // the passsword
+
+			// Questa licenza dovrebbe essere valida fino al 20/11/2015
+			ValidateAKey.Key = "KFBFR-SZNVH-DGVXL-RTCLT"; // enter a valid key
+			Assert.IsFalse( ValidateAKey.IsExpired );
+			Assert.IsTrue( ValidateAKey.IsValid );
+
+			// Questa chiave non è valida (cioè è sbagliata)
+			ValidateAKey.Key = "yFBFR-SZNVH-DGVXL-RTCLT";
+			Assert.IsFalse( ValidateAKey.IsValid );
+
+			// Questa chiave è corretta ma è scaduta
+			ValidateAKey.Key = "LREYZ-AAIDV-RLGRK-EVPFQ";
+			Assert.IsTrue( ValidateAKey.IsExpired );
+			Assert.IsTrue( ValidateAKey.IsExpired );
+
+			
+		}
+
+	}
+}
---
+++ Digiphoto.Lumen.Core/src/Licensing/RegistryLicense.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Digiphoto.Lumen.Licensing {
+
+	public class RegistryLicense : License {
+
+		private SKGL.Validate _validate;
+
+
+		public RegistryLicense( string licenceKey ) {
+
+			if( licenceKey == null )
+				throw new NullReferenceException( "The licensed type reference may not be null." );
+
+			_validate = new SKGL.Validate();
+			_validate.Key = licenceKey;
+
+			if( ! _validate.IsValid )
+				throw new NullReferenceException( "The licence code is not valid" );
+		}
+
+		public override string LicenseKey {
+			get {
+				return this._validate.Key;
+			}
+		}
+
+		public bool IsExpired {
+			get {
+				return _validate.IsExpired;
+			}
+		}
+
+		public bool IsOnRightMachine {
+			get {
+				return _validate.IsOnRightMachine;
+			}
+		}
+
+		public DateTime CreationDate {
+			get {
+				return _validate.CreationDate;
+			}
+		}
+
+		public int DaysLeft {
+			get {
+				return _validate.DaysLeft;
+			}
+		}
+
+		public DateTime ExpireDate {
+			get {
+				return _validate.ExpireDate;
+			}
+		}
+
+		public override void Dispose() {
+		}
+	}
+}
