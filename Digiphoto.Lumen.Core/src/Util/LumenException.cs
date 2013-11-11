e68aa161fe6c2839c90c202fb3ee9ee5abf90eb2
Luca Bernardini <54762057+digiPHOTO-it@users.noreply.github.com>
2013-11-11 08:36:25 +0100

    Eccezione personalizzata per problemi interni Lumen.

---
+++ Digiphoto.Lumen.Core/src/Util/LumenException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Digiphoto.Lumen.Util {
+
+	public class LumenException : Exception {
+
+		public LumenException() : base() {
+		}
+
+		public LumenException( string message ) : base( message ) {
+		}
+
+		public LumenException( string message, Exception innerException ): base( message, innerException ) {
+		}
+
+	}
+}
