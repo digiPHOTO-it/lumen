6ecb24a1130111d13df9870fc6f049b47afaf50f
Luca Bernardini <lucasimone.bernardini@gmail.com>
2014-01-03 22:55:49 +0100

    Task:#219 Nel fotoritocco se avanzo con SPACE quando arrivo in fondo alla lista spostarsi alla pagina successiva.
    Introdotto un servizio EntityRepository delle Fotografie. Mi serve quando torno
    originale una foto.

---
+++ Digiphoto.Lumen.Core/src/Servizi/EntityRepository/FotografieRepositorySrvImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Digiphoto.Lumen.Model;
+using Digiphoto.Lumen.Core.Database;
+using  System.Data.Entity.Core.Objects;
+
+namespace Digiphoto.Lumen.Servizi.EntityRepository {
+
+	public class FotografieRepositorySrvImpl : EntityRepositorySrvImpl<Fotografia> {
+
+		public FotografieRepositorySrvImpl() : base() {
+		}
+
+		public override Fotografia getById( object oid ) {
+			Guid id = (Guid)oid;
+			return UnitOfWorkScope.currentDbContext.Fotografie.SingleOrDefault( f => f.id == id );
+		}
+	}
+}
