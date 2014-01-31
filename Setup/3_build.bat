526bf496d768db4fcb2216341ea30e4cc09367dd
Rilasci (lucasimone bernardini <Rilasci (lucasimone.bernardini@gmail.com)>
2014-01-31 23:17:04 +0100

    Prove di setup 0.19 con un sacco di refactoring da testare. Non so se funzionaranno.

--- Setup/3_build.bat
+++ Setup/3_build.bat
@@ -1,4 +1,10 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+
+
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call "C:\Program Files\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat
+
+set EnableNuGetPackageRestore=true
+
 
 rem scarico eventuali pacchetti necessari
 ..\.nuget\nuget install packages.config -o ..\packages
