1865e97c432d5de289f2e96a68582cb5cc9e8652
Edward84 <Edward84@localhost>
2014-02-01 17:20:08 +0100

    Issue #224 Azioni rapide in fotoritocco si spacca.

---
+++ Digiphoto.Lumen.UI/Converters/ListAzioniRapideVisibilityConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+using System.ComponentModel;
+using Digiphoto.Lumen.Model;
+using System.Collections;
+using Digiphoto.Lumen.UI.Mvvm.MultiSelect;
+
+namespace Digiphoto.Lumen.UI.Converters {
+
+	public class ListAzioniRapideVisibilityConverter : IValueConverter
+	{
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture ) {
+			if (value == null)
+			{
+				return Visibility.Collapsed;
+			}
+			if (value is MultiSelectCollectionView<Fotografia>)
+			{
+				if (((MultiSelectCollectionView<Fotografia>)value).Count > 1)
+				{
+					return Visibility.Visible;
+				}
+				else
+				{
+					return Visibility.Collapsed;
+				}
+			}
+			
+			return Visibility.Collapsed;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter,	CultureInfo culture ) {
+			throw new NotImplementedException();
+		}
+	}
+}
