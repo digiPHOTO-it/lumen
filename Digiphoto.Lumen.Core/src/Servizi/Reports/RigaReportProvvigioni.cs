06a2aeb24219fe748c87af33e34899d3b5c13e9e
Luca Bernardini <54762057+digiPHOTO-it@users.noreply.github.com>
2014-02-16 09:13:57 +0100

    Task 242 Report per provvigioni fotografo.

---
+++ Digiphoto.Lumen.Core/src/Servizi/Reports/RigaReportProvvigioni.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Digiphoto.Lumen.Servizi.Reports {
+	
+	public class RigaReportProvvigioni {
+
+		public string nomeFotografo { get; set; }
+		public decimal incasso { get; set; }
+		public decimal incassoStampe { get; set; }
+		public decimal incassoMasterizzate { get; set; }
+		public int contaStampe { get; set; }
+		public int contaMasterizzate { get; set; }
+	}
+}
