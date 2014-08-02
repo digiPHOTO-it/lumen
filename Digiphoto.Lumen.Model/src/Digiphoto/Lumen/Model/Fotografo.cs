81d8a67af1b628214400c7650641c000620e0cd6
Luca Bernardini <54762057+digiPHOTO-it@users.noreply.github.com>
2014-08-02 14:23:58 +0200

    l' ID del fotografo, ora lo auto genero. Inoltre ho eliminato il limite di 4 caratteri. Issue #254
    Tradotto le toolbar in italiano (i pulsanti di azione sul dataentry)

--- Digiphoto.Lumen.Model/src/Digiphoto/Lumen/Model/Fotografo.cs
+++ Digiphoto.Lumen.Model/src/Digiphoto/Lumen/Model/Fotografo.cs
@@ -16,12 +16,12 @@ namespace Digiphoto.Lumen.Model {
 
 			List<ValidationResult> errors = new List<ValidationResult>();
 
-			if( this.cognomeNome == null || this.cognomeNome.Length < 1 || this.cognomeNome.Length > 100 ) {
+			if( String.IsNullOrEmpty( this.cognomeNome ) || this.cognomeNome.Length > 100 ) {
 				ValidationResult vr = new ValidationResult( "Cognome e nome fotografo non valido", new string [] { "cognomeNome" } );
 				errors.Add( vr );
 			}
 
-			if( this.id == null || (this.id.Length < 4 || this.id.Length > 100) ) {
+			if( this.id == null || this.id.Length > 16 ) {
 				ValidationResult vr = new ValidationResult( "Id fotografo non valido", new string [] { "id" } );
 				errors.Add( vr );
 			}
