676e2a3777aaad1d5bd1dd4df92e89ea45726986
Luca Bernardini <Luca Bernardini@localhost>
2015-12-13 21:00:31 +0100

    Aggiunta funzione per aprire il dialogo di windows per configurare il monitor Issue #356
    Gi? che c'ero ho eliminato un p? di funzioni vecchie che erano solo disabilitate.
    Ho fatto un p? di pulizia.

--- Digiphoto.Lumen.UI/Pubblico/GestioneGeometria/GestSlideShowView.xaml.cs
+++ Digiphoto.Lumen.UI/Pubblico/GestioneGeometria/GestSlideShowView.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using Digiphoto.Lumen.UI.Adorners;
 using Digiphoto.Lumen.Config;
 using Digiphoto.Lumen.UI.Mvvm;
+using System.Diagnostics;
 
 namespace Digiphoto.Lumen.UI.Pubblico
 {
@@ -53,109 +54,14 @@ namespace Digiphoto.Lumen.UI.Pubblico
 
 		#endregion
 
-		private void Monitor_Loaded(object sender, RoutedEventArgs e)
-		{
-			this.MouseLeftButtonDown += new MouseButtonEventHandler(Monitor_MouseLeftButtonDown);
-			this.MouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
-			this.MouseMove += new MouseEventHandler(Monitor_MouseMove);
-			this.MouseLeave += new MouseEventHandler(Monitor_MouseLeave);
+		
 
-			monitor.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(myCanvas_PreviewMouseLeftButtonDown);
-			monitor.PreviewMouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
-		}
+		private void proprietaMonitorButton_Click(object sender, RoutedEventArgs e) {
 
-		// Handler for drag stopping on leaving the window
-		void Monitor_MouseLeave(object sender, MouseEventArgs e)
-		{
-			StopDragging();
-			e.Handled = true;
-		}
-
-		// Handler for drag stopping on user choise
-		void DragFinishedMouseHandler(object sender, MouseButtonEventArgs e)
-		{
-			StopDragging();
-			e.Handled = true;
-		}
-
-		// Method for stopping dragging
-		private void StopDragging()
-		{
-			if (_isDown)
-			{
-				_isDown = false;
-				_isDragging = false;
-			}
-		}
-
-		// Hanler for providing drag operation with selected element
-		void Monitor_MouseMove(object sender, MouseEventArgs e)
-		{
-			if (_isDown)
-			{
-				if ((_isDragging == false) &&
-					((Math.Abs(e.GetPosition(monitor).X - _startPoint.X) > SystemParameters.MinimumHorizontalDragDistance) ||
-					(Math.Abs(e.GetPosition(monitor).Y - _startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)))
-					_isDragging = true;
-
-				if (_isDragging)
-				{
-					Point position = Mouse.GetPosition(monitor);
-					Canvas.SetTop(selectedElement, position.Y - (_startPoint.Y - _originalTop));
-					Canvas.SetLeft(selectedElement, position.X - (_startPoint.X - _originalLeft));
-				}
-			}
-		}
-
-		// Handler for clearing element selection, adorner removal
-		void Monitor_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-		{
-			if (selected)
-			{
-				selected = false;
-				if (selectedElement != null)
-				{
-					aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
-					selectedElement = null;
-				}
-			}
-		}
-
-		// Handler for element selection on the canvas providing resizing adorner
-		void myCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-		{
-			// Remove selection on clicking anywhere the window
-			if (selected)
-			{
-				selected = false;
-				if (selectedElement != null)
-				{
-					// Remove the adorner from the selected element
-					if (aLayer!=null && aLayer.GetAdorners(selectedElement)!=null)
-					{
-						aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
-						selectedElement = null;
-					}
-				}
-			}
-
-			// If any element except canvas is clicked, 
-			// assign the selected element and add the adorner
-			if (e.Source != monitor)
-			{
-				_isDown = true;
-				_startPoint = e.GetPosition(monitor);
-
-				selectedElement = e.Source as UIElement;
-
-				_originalLeft = Canvas.GetLeft(selectedElement);
-				_originalTop = Canvas.GetTop(selectedElement);
-
-				aLayer = AdornerLayer.GetAdornerLayer(selectedElement);
-				aLayer.Add(new ResizingAdorner(selectedElement));
-				selected = true;
-				e.Handled = true;
-			}
+			// String path = Environment.GetFolderPath( Environment.SpecialFolder.System );
+			String exe = "rundll32.exe";
+			String arguments = "shell32.dll,Control_RunDLL desk.cpl,,3";
+            Process.Start( exe, arguments );
 		}
 	}
 }
