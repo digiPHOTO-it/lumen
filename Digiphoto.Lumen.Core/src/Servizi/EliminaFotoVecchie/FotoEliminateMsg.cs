97efb69d4f5b41cc8db8eee2fe9a9a4fbc3b3dae
Luca Bernardini <Luca Bernardini@localhost>
2016-09-25 20:10:13 +0200

    vedi commit precedente

---
+++ Digiphoto.Lumen.Core/src/Servizi/EliminaFotoVecchie/FotoEliminateMsg.cs
@@ -0,0 +1,21 @@
+﻿using Digiphoto.Lumen.Eventi;
+using Digiphoto.Lumen.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digiphoto.Lumen.Servizi.EliminaFotoVecchie {
+	
+public class FotoEliminateMsg : Messaggio {
+
+		public FotoEliminateMsg( object sender ) : base( sender ) {
+		}
+
+		public IEnumerable<Fotografia> listaFotoEliminate {
+			get;
+			set;
+		}
+	}
+}
---
+++ Digiphoto.Lumen.Core/src/Util/ExtensionMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digiphoto.Lumen.Util {
+
+	public static class ExtensionMethods {
+
+		public static T[] ConvertToArray<T>( this IEnumerable<T> enumerable ) {
+			if( enumerable == null )
+				throw new ArgumentNullException( "enumerable" );
+
+			return enumerable as T[] ?? enumerable.ToArray();
+		}
+
+		/// <summary>
+		/// Metodo di estensione statica.
+		/// Si può usare su di un qualunque oggetto per farne una copia.
+		/// In pratica si serializza in memorie e si ri-istanzia un altro oggetto 
+		/// che diventa un clone del primo
+		/// 
+		/// In pratica realizza una Deep Copy che è il contrario della ShallowCopy
+		/// </summary>
+		/// <typeparam name="T">La classe dell'oggtto da copiare</typeparam>
+		/// <param name="source">L'oggetto da copiare</param>
+		/// <returns></returns>
+		public static T deepCopy<T>( this T source ) {
+			var isNotSerializable = !typeof( T ).IsSerializable;
+			if( isNotSerializable )
+				throw new ArgumentException( "The type must be serializable.", "source" );
+
+			var sourceIsNull = ReferenceEquals( source, null );
+			if( sourceIsNull )
+				return default( T );
+
+			var formatter = new BinaryFormatter();
+			using( var stream = new MemoryStream() ) {
+				formatter.Serialize( stream, source );
+				stream.Seek( 0, SeekOrigin.Begin );
+				return (T)formatter.Deserialize( stream );
+			}
+		}
+
+		public static IEnumerable<T> ToIEnumerable<T>( this IEnumerator<T> enumerator ) {
+			while( enumerator.MoveNext() ) {
+				yield return enumerator.Current;
+			}
+		}
+	}
+}
