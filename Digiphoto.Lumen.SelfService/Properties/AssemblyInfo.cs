7c47e330d68027e95a158554a9d6c138dae0ee96
Luca Bernardini <Luca Bernardini@localhost>
2016-12-13 08:07:52 +0100

    Ora il tablet risce a parlare con il server in rete. Risolti problema di autenticazione Issue 453.
    Il prototipo della applicazione mobile ? usabile a tutti gli effetti.
    
    Modificato anche il num. di release che diventer? 2.1

--- Digiphoto.Lumen.SelfService/Properties/AssemblyInfo.cs
+++ Digiphoto.Lumen.SelfService/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "Digiphoto.Lumen.SelfService" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Lumen Self Service - WCF service library" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "digiPHOTO.it" )]
 [assembly: AssemblyProduct( "Digiphoto.Lumen.SelfService" )]
-[assembly: AssemblyCopyright( "Copyright ©  2016" )]
+[assembly: AssemblyCopyright( "Copyright © digiPHOTO.it 2014-2016" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "2.1.0.1" )]
+[assembly: AssemblyFileVersion( "2.1.0.1" )]
