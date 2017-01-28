5f227962275f104704b02bab08ecd7906dd2de77
Edward84 <Edward84@localhost>
2017-01-28 18:54:50 +0100

    Prima versione Self

---
+++ Digiphoto.Lumen.SelfService.MobileUI/Main/SelfMainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Digiphoto.Lumen.SelfService.MobileUI.SelfServiceReference;
+using System;
+using System.Windows;
+using System.Windows.Input;
+namespace Digiphoto.Lumen.SelfService.MobileUI {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class SelfMainWindow : Window {
+
+        SelfServiceClient ssClient;
+
+        // Gestisco l'autoExit dalle schermate
+        private Point _mousePoint = new Point(0, 0);
+        private System.Windows.Threading.DispatcherTimer _MouseTicker = new System.Windows.Threading.DispatcherTimer();
+
+        public static bool isShowLogo = false;
+        public static bool isShowCarrelli = false;
+        public static bool isShowSlideShow = false;
+
+        public SelfMainWindow() {
+			
+			InitializeComponent();
+            this.DataContext = this;
+
+            this.TouchMove += new EventHandler<TouchEventArgs>(TouchableThing_TouchMove);
+
+            // Mi connetto con il servizio SelfService.
+            ssClient = new SelfServiceClient();
+            ssClient.Open();
+
+            // AutoExit
+            _MouseTicker.Tick += new EventHandler(dispatcherTimer_MouseTicker);
+            _MouseTicker.Interval = new TimeSpan(0, 0, 0, 10);
+            _MouseTicker.Start();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowLogo();
+        }
+
+        private void TouchableThing_TouchMove(object sender, TouchEventArgs e)
+        {
+            if(!isShowCarrelli)
+                ShowCarrelli();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                ShowCarrelli();
+            }
+        }
+
+        private void Window_Closed( object sender, EventArgs e ) {
+			if( ssClient != null ) {
+				ssClient.Close();
+				ssClient = null;
+			}
+		}
+
+        #region dispatcherTimer
+
+        private void dispatcherTimer_MouseTicker(object sender, EventArgs e)
+        {
+            if (_mousePoint != Mouse.GetPosition(this))
+            {
+                _mousePoint.X = Mouse.GetPosition(this).X;
+                _mousePoint.Y = Mouse.GetPosition(this).Y;
+            }
+            else
+            {
+                ShowLogo();
+            }
+        }
+
+        #endregion
+
+        #region Show
+
+        private void ShowLogo()
+        {
+            this.Content = new Logo(ssClient);
+        }
+
+        private void ShowCarrelli()
+        {
+            this.Content = new Carrelli(ssClient);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Digiphoto.Lumen.SelfService.MobileUI/Servizi/FotoSrv.cs
@@ -0,0 +1,141 @@
+﻿using Digiphoto.Lumen.SelfService.MobileUI.SelfServiceReference;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Digiphoto.Lumen.SelfService.MobileUI.Servizi
+{
+    public class FotoSrv
+    {
+        private static FotoSrv instance;
+
+        private readonly String selfPath = @"C:\Self\";
+        private String proviniPath = "Provini";
+        private String risultantePath;
+        private Guid idCarrello;
+
+        private FotoSrv()
+        {
+            // Creo la cartella che conterrà le foto
+            if (System.IO.Directory.Exists(selfPath))
+            {
+                deleteDirectory(selfPath);
+            }
+            if (!System.IO.Directory.Exists(selfPath))
+            {
+                Directory.CreateDirectory(selfPath);
+            }
+        }
+
+        public static FotoSrv Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new FotoSrv();
+                }
+                return instance;
+            }
+        }
+
+        public void setCarello(Guid id)
+        {
+            if (!idCarrello.Equals(id))
+            {
+                if (!idCarrello.Equals(Guid.Empty))
+                {
+                    String pathCarrelloOld = Path.Combine(selfPath, idCarrello.ToString());
+                    deleteDirectory(pathCarrelloOld);
+                }
+                String pathCarrelloNew = Path.Combine(selfPath, id.ToString());
+                String pathProvini = Path.Combine(pathCarrelloNew, proviniPath);
+
+                Directory.CreateDirectory(pathCarrelloNew);
+                Directory.CreateDirectory(pathProvini);
+
+                risultantePath = pathCarrelloNew;
+                idCarrello = id;
+            }
+        }
+
+        public BitmapImage loadPhoto(SelfServiceClient ssClient, string quale, Guid fotografiaId)
+        {
+            byte[] bytes = null;
+            bool scriviFile = false;
+            String photoName = fotografiaId.ToString();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            String _path = Path.Combine(selfPath, System.IO.Path.ChangeExtension(photoName, ".jpg"));
+
+            if (quale == "Provino")
+            {    
+                _path = Path.Combine(risultantePath, proviniPath, System.IO.Path.ChangeExtension(photoName, ".jpg"));
+                if (!System.IO.File.Exists(_path))
+                {
+                    bytes = ssClient.getImageProvino(fotografiaId);
+                    scriviFile = true;
+                }
+            }
+            else if (quale == "Logo")
+            {
+                _path = Path.Combine(selfPath, System.IO.Path.ChangeExtension("Logo", ".jpg"));
+                if (!System.IO.File.Exists(_path))
+                {
+                    bytes = ssClient.getImageLogo();
+                    scriviFile = true;
+                }
+            }
+            else if (quale == "Risultante")
+            {
+                _path = Path.Combine(risultantePath, System.IO.Path.ChangeExtension(photoName, ".jpg"));
+                if (!System.IO.File.Exists(_path))
+                {
+                    bytes = ssClient.getImage(fotografiaId);
+                    scriviFile = true;
+                }
+            }
+            
+            // Salvo il file su disco
+            if (scriviFile)
+            {
+                File.WriteAllBytes(_path, bytes);
+            }
+            BitmapImage tempImage = new BitmapImage();
+            try
+            {
+                tempImage.BeginInit();
+                tempImage.UriSource = new Uri(_path);
+                tempImage.CacheOption = BitmapCacheOption.OnLoad;
+                tempImage.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                tempImage.EndInit();
+                tempImage.Freeze();
+            }
+            catch (Exception ex) { ex.ToString(); }
+            sw.Stop();
+            Console.WriteLine("File={0} - ScriviFile={1} - Elapsed={2}", _path, scriviFile, sw.Elapsed);
+            return tempImage;
+        }
+
+        /// <summary>
+        /// Consente di eliminare tutte le foto contenute nel path
+        /// </summary>
+        /// <param name="pathCartella"></param>
+        public void deleteDirectory(String pathCartella)
+        {
+            foreach (string directoryPath in Directory.GetFiles(pathCartella))
+            {
+                String nomeFile = Path.GetFileName(directoryPath);
+                //Elimino gli attributi solo lettura file nascosti
+                File.SetAttributes(directoryPath, File.GetAttributes(directoryPath) & ~FileAttributes.Hidden);
+                //Elimino gli attributi solo lettura                                
+                File.SetAttributes(directoryPath, File.GetAttributes(directoryPath) & ~(FileAttributes.Archive | FileAttributes.ReadOnly));
+            }
+            String dirDataPath = Path.GetDirectoryName(pathCartella);
+            Directory.Delete(pathCartella, true);
+        }
+
+    }
+}
