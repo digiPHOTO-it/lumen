8d1851e93ea563d950316e989f2ac371682cd50c
Edward84 <Edward84@localhost>
2017-02-05 11:48:57 +0100

    Modificato meccanismo di Time Self

--- Digiphoto.Lumen.SelfService.MobileUI/Logo/Logo.xaml.cs
+++ Digiphoto.Lumen.SelfService.MobileUI/Logo/Logo.xaml.cs
@@ -3,6 +3,7 @@ using Digiphoto.Lumen.SelfService.MobileUI.Servizi;
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Digiphoto.Lumen.SelfService.MobileUI
 {
@@ -11,13 +12,16 @@ namespace Digiphoto.Lumen.SelfService.MobileUI
     /// </summary>
     public partial class Logo : UserControl
     {
+        private SelfMainWindow main;
+
         private SelfServiceClient ssClient;
 
-        public Logo(SelfServiceClient ssClient)
+        public Logo(SelfMainWindow main, SelfServiceClient ssClient)
         {
             InitializeComponent();
 
             this.ssClient = ssClient;
+            this.main = main;
 
             imageFoto.Source = FotoSrv.Instance.loadPhoto(ssClient, "Logo", Guid.Empty);
 
@@ -46,5 +50,17 @@ namespace Digiphoto.Lumen.SelfService.MobileUI
 
             Application.Current.Shutdown();
         }
+
+        private void Go(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Go();
+        }
+
+        private void Go()
+        {
+            if (!SelfMainWindow.isShowCarrelli)
+                main.ContentArea.Content = new Carrelli(main, ssClient);
+        }
+    
     }
 }
