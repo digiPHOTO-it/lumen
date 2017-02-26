567eb9bf9d8d77c7a05dd94e1fcd0e1ae181c2b8
Edward84 <Edward84@localhost>
2017-02-26 10:05:04 +0100

    Issue #461 Pulsanti self service

---
+++ Digiphoto.Lumen.SelfService.MobileUI/Converters/MiPiaceBollConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Digiphoto.Lumen.SelfService.MobileUI.Converters
+{
+	/// <summary>
+	/// Questo convertitore mi serve per convertire un booleano 
+	/// </summary>
+	public class MiPiaceBoolConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+
+			// Carico una icona dal file delle risorse
+			bool? miPiace = (bool?)value;
+			return miPiace == true ? true : false;
+        }
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			bool? miPiace = (bool?)value;
+			return miPiace == true ? true : false;
+		}
+	}
+}
