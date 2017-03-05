2c59d971865f6d97791cb821ac731ea8602921b6
Luca Bernardini <Luca Bernardini@localhost>
2017-03-05 18:45:28 +0100

    Riscritta la query di ricerca foto, ora non uso pi? Linq-to-Entities ma uso l'SQL diretto (Myqsl e SqlLite sembrano uguali). Questo per risolvere il problema della lentezza in rete. Issue #472

--- Digiphoto.Lumen.GestoreConfigurazione.UI/Properties/AssemblyInfo.cs
+++ Digiphoto.Lumen.GestoreConfigurazione.UI/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2.0.0.610" )]
-[assembly: AssemblyFileVersion( "2.0.0.610" )]
+[assembly: AssemblyVersion( "2.0.0.611" )]
+[assembly: AssemblyFileVersion( "2.0.0.611" )]
 
 [assembly: NeutralResourcesLanguageAttribute("it-IT")]
