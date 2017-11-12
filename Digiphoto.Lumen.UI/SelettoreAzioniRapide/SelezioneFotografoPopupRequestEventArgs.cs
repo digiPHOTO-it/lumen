d154976026c38289d0d64bc293046ab39d0efb0c
Luca Bernardini <Luca Bernardini@localhost>
2017-11-12 10:23:15 +0100

    Introdotte due nuovi classi Una per gestire le richieste di apertura popup : OpenPopupRequestEventArgs (In questo modo posso usare sempre la stessa e non sono costretto a creare una classe diverse per ogni messaggio di apertura).

--- Digiphoto.Lumen.UI/SelettoreAzioniRapide/SelezioneFotografoPopupRequestEventArgs.cs
+++ Digiphoto.Lumen.UI/SelettoreAzioniRapide/SelezioneFotografoPopupRequestEventArgs.cs
@@ -1,4 +1,5 @@
 ﻿using Digiphoto.Lumen.Model;
+using Digiphoto.Lumen.UI.Mvvm.Event;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Digiphoto.Lumen.UI.SelettoreAzioniRapide {
 
-	public class SelezioneFotografoPopupRequestEventArgs : EventArgs {
+	public class SelezioneFotografoPopupRequestEventArgs : OpenPopupRequestEventArgs {
 
 		public SelezioneFotografoPopupRequestEventArgs( Fotografia fotoFaccia ) {
 			this.fotoFaccia = fotoFaccia;
