85e2324e6805dd1efcd97098d740045e976e4ca9
Luca Bernardini <Luca Bernardini@localhost>
2018-06-30 14:53:15 +0200

    Migliorie OnRide ora ? funzionante con una bella dignit?. Sarebbe usabile in produzione.

---
+++ Digiphoto.Lumen.OnRide.SimulaScatti/SimulaScatti.bat
@@ -0,0 +1,5 @@
+PUSHD "C:\Users\bluca\Documents\Visual Studio 2015\Projects\lumen\Digiphoto.Lumen.OnRide.SimulaScatti\bin\Debug"
+
+Digiphoto.Lumen.OnRide.SimulaScatti.exe "C:\Users\bluca\Pictures\2018_06_il_Cammino" "D:\OnRideIn" 10
+
+POPD
--- Digiphoto.Lumen.OnRide.UI/Main/MainWindow.xaml.cs
+++ Digiphoto.Lumen.OnRide.UI/Main/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
-﻿using System.Windows;
-
-
+﻿using Digiphoto.Lumen.OnRide.UI.Model;
+using Digiphoto.Lumen.UI.Mvvm;
+using System;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Digiphoto.Lumen.OnRide.UI {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow : Window, IDialogProvider {
 
 		public MainWindow() {
 
@@ -14,7 +16,91 @@ namespace Digiphoto.Lumen.OnRide.UI {
 
 			this.DataContext = new MainWindowViewModel();
 
+			viewModel.dialogProvider = this;
 		}
+
+		MainWindowViewModel viewModel {
+			get {
+				return (MainWindowViewModel)this.DataContext;
+			}
+		}
+
+
+		private void boxHeaderEliminare_Click( object sender, RoutedEventArgs e ) {
+
+			// In base al mio valore, valorizzo tutte le righe eventuali
+			for( int ii = 0; ii < viewModel.fotoItemsCW.Count; ii++ ) {
+				FotoItem item = (FotoItem) viewModel.fotoItemsCW.GetItemAt( ii );
+				item.daEliminare = (boxHeaderEliminare.IsChecked == true);
+			}
+		}
+
+		private void boxHeaderTaggare_Click( object sender, RoutedEventArgs e ) {
+
+			// In base al mio valore, valorizzo tutte le righe eventuali
+			for( int ii = 0; ii < viewModel.fotoItemsCW.Count; ii++ ) {
+				FotoItem item = (FotoItem)viewModel.fotoItemsCW.GetItemAt( ii );
+				item.daTaggare = (boxHeaderTaggare.IsChecked == true);
+			}
+		}
+
+		#region IDIalogProvider
+
+		/// <summary>
+		/// Visualizza un messaggio
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="title"></param>
+		/// <param name="afterHideCallback"></param>
+		public void ShowError( string message, string title, Action afterHideCallback ) {
+
+			var risultato = MessageBox.Show( this, message, title, MessageBoxButton.OK, MessageBoxImage.Error );
+			if( afterHideCallback != null )
+				afterHideCallback();
+		}
+
+		public void ShowMessage( string message, string title ) {
+			MessageBox.Show( this, message, title, MessageBoxButton.OK, MessageBoxImage.Information );
+		}
+		
+		/// <summary>
+		/// Chiedo conferma SI/NO.
+		/// Chiamo la callback passando TRUE se l'utente ha scelto SI.
+		/// </summary>
+		public void ShowConfirmation( string message, string title, Action<bool> afterHideCallback ) {
+			var tastoPremuto = MessageBox.Show( this, message, title, MessageBoxButton.YesNo, MessageBoxImage.Question );
+			afterHideCallback( tastoPremuto == MessageBoxResult.Yes );
+		}
+
+		/// <summary>
+		/// Chiedo conferma SI/NO/ANNULLA.
+		/// Chiamo la callback passando TRUE se l'utente ha scelto SI.
+		/// </summary>
+		public void ShowConfirmationAnnulla( string message, string title, Action<MessageBoxResult> afterHideCallback ) {
+			var tastoPremuto = MessageBox.Show( this, message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Question );
+			afterHideCallback( tastoPremuto );
+		}
+
+
+		private bool _attenderePrego;
+		public bool attenderePrego {
+			get {
+				return _attenderePrego;
+			}
+			set {
+
+				if( _attenderePrego != value ) {
+					_attenderePrego = value;
+
+					if( value == true )
+						this.Cursor = Cursors.Wait;
+					else
+						this.Cursor = Cursors.AppStarting;  // normale
+				}
+			}
+		}
+
+		#endregion IDIalogProvider
 
 	}
 }
