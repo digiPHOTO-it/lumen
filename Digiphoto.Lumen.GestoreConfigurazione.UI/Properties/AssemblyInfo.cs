0c9989d04877fefe102a96c451fd9af37d5d47ce
Luca Bernardini <webmaster@digiphoto.it>
2019-01-11 07:30:56 +0100

    modificato assembly version 3.0-alpha4

--- Digiphoto.Lumen.GestoreConfigurazione.UI/Properties/AssemblyInfo.cs
+++ Digiphoto.Lumen.GestoreConfigurazione.UI/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "3.0.0.503" )]
+[assembly: AssemblyVersion( "3.0.0.504" )]
 
 [assembly: NeutralResourcesLanguageAttribute("it-IT")]
