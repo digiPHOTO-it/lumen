3103ad41559551a7673f0e58a3d1500bb7595c23
Luca Bernardini <webmaster@digiphoto.it>
2019-04-25 20:24:32 +0200

    Prima bozza funzionante della nuova funzionalit? per le impronte digitali Per il momento ho implementato solo una funzione sul server per memorizzare le impronte anche nel db.

---
+++ Digiphoto.Lumen.Model/src/Digiphoto/Lumen/Model/Ospite.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Digiphoto.Lumen.Model {
+
+
+	[MetadataType( typeof( Ospite ) )]
+	[Serializable]
+	[Table( "Ospiti" )]
+	public partial class Ospite {
+
+		public Ospite() {
+		}
+
+		#region Attributi
+
+		[Key]
+		[DatabaseGenerated( DatabaseGeneratedOption.None )]
+		public int id { get; set; }
+
+		[Required]
+		public byte [] impronta { get; set; }
+
+		public string nome { get; set; }
+
+		#endregion Attributi
+
+		#region Uguaglianza
+
+		public override int GetHashCode() {
+			return id.GetHashCode();
+		}
+
+		public override bool Equals( object obj ) {
+
+			bool sonoUguali = false;
+
+			if( obj is Ospite ) {
+
+				Ospite altro = (Ospite)obj;
+				sonoUguali = this.id == altro.id;
+			}
+
+			return sonoUguali;
+		}
+
+		#endregion Uguaglianza
+
+
+	}
+}
