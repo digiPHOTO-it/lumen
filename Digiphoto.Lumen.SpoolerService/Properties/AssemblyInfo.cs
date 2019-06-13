81084060b83659acd48a38b09295782f2cc34112
Luca Bernardini <webmaster@digiphoto.it>
2019-06-13 23:56:54 +0200

    Stampe sul server tramite comunicazione WCF
    Preparo rel. 3.2-rc1

---
+++ Digiphoto.Lumen.SpoolerService/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "Digiphoto.Lumen.SpoolService" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Digiphoto.Lumen.SpoolService" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "901377ee-96ca-4254-a15d-2bd3450aa9f2" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "3.2.0.501" )]
---
+++ Digiphotot.Lumen.UI.Test/src/PrintServerTest/PrintServiceClientWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Digiphoto.Lumen.Model;
+using Digiphoto.Lumen.Servizi.Stampare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Digiphoto.Lumen.UI.Test.src.PrintServerTest {
+	/// <summary>
+	/// Interaction logic for PrintServiceClientWindow.xaml
+	/// </summary>
+	public partial class PrintServiceClientWindow : Window {
+		public PrintServiceClientWindow()
+		{
+			InitializeComponent();
+
+		
+			Guid fotografiaId = Guid.NewGuid();
+
+			// psc.AccodaStampaFoto( fotografiaId );
+
+		}
+	}
+}
