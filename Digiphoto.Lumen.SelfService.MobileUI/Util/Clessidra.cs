3edee2f6211058da5f898c36fbd4fc8b63f8e3a1
Luca Bernardini <webmaster@digiphoto.it>
2019-07-20 15:58:57 +0200

    Aggiunta clessidra per quando si clicca sul pulsante start. Inspiegabilmente ci mette 6 secondi (verificato in diversi villaggi)

---
+++ Digiphoto.Lumen.SelfService.MobileUI/Util/Clessidra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Digiphoto.Lumen.SelfService.MobileUI.Util {
+	public class Clessidra : IDisposable {
+
+		private Cursor _previousCursor;
+
+		public Clessidra() {
+			_previousCursor = Mouse.OverrideCursor;
+
+			Mouse.OverrideCursor = Cursors.Wait;
+		}
+
+		#region IDisposable Members
+
+		public void Dispose() {
+			Mouse.OverrideCursor = _previousCursor;
+		}
+
+		#endregion
+	}
+}
