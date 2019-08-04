97c18cde2c6d7c570e1a8554309d48d77bb7395b
Luca Bernardini <webmaster@digiphoto.it>
2019-08-04 13:18:29 +0200

    La promozone 3 (prodotto A regala prodotto B) era stata pensata per funzionare solo sulle stampe e non sui file digitali. Ora l'ho estesa anche ai file. Gi? che c'ero ho corretto un piccolo bug nel caso di applicazione su righe gi? sottoposte alla promo num.1
    Issue #614

--- Digiphoto.Lumen.Core/src/Servizi/Vendere/CalcolatorePromoProdXProd.cs
+++ Digiphoto.Lumen.Core/src/Servizi/Vendere/CalcolatorePromoProdXProd.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using log4net;
+using Digiphoto.Lumen.Database;
 
 namespace Digiphoto.Lumen.Core.Servizi.Vendere {
 
@@ -10,27 +11,25 @@ namespace Digiphoto.Lumen.Core.Servizi.Vendere {
 		private Dictionary<string, int> _mappaDaElargireStampe = new Dictionary<string, int>();
 		private Dictionary<string, int> _mappaTotStampe;
 		private int _daElargireFile;
-		private PromoProdXProd _promoProdXProd;
 
 		protected static readonly ILog _giornale = LogManager.GetLogger( typeof( CalcolatorePromoProdXProd ) );
 
 		public Carrello Applica( Carrello cin, Promozione promo, PromoContext contestoDiVendita ) {
 
-			this._promoProdXProd = (PromoProdXProd)promo;
-	
-			// Non entro in promozione: esco subito
-			if( !promo.attivaSuStampe )
-				return cin;
-			
+			PromoProdXProd _promoProdXProd = (PromoProdXProd)promo;
+
+			OrmUtil.forseAttacca<PromoProdXProd>( ref _promoProdXProd );
+
+			// NOTA: non controllo più il flag di applicazione stampe/file che c'è sul database.
+			//       tanto è il prodotto di innesco che mi guida.
+
 			var qta = cin.righeCarrello
-				.Where( r => r.discriminator == RigaCarrello.TIPORIGA_STAMPA &&
-						r.prodotto.Equals( _promoProdXProd.prodottoInnesco ) )
+				.Where( r => r.prezzoLordoUnitario > 0 && r.prodotto.Equals( _promoProdXProd.prodottoInnesco ) )
 				.Sum( r2 => r2.quantita );
 
 			// Non entro in promozione: esco subito
 			if( qta < _promoProdXProd.qtaInnesco )
 				return cin;
-
 
 			// Ok la quantità è sufficiente per cascare nella promo.
 			// Ora agisco sul carrello
@@ -40,18 +39,24 @@ namespace Digiphoto.Lumen.Core.Servizi.Vendere {
 			var qtaElarg = multiploRegali * (_promoProdXProd.qtaElargito);
 			_giornale.Debug( "devo elargire " + qtaElarg + " omaggio" );
 
-			// Ora itero le righe del carrello con quel tipo
-			var righeOma = cin.righeCarrello
-				.Where( r => r.prodotto.Equals( _promoProdXProd.prodottoElargito ) );
-
 			bool elargito = false;
-			foreach( RigaCarrello riga in righeOma ) {
 
-				if( qtaElarg >= riga.quantita ) {
-					riga.sconto = riga.prezzoLordoUnitario;
-					qtaElarg -= riga.quantita;
+			// -- Ora itero le righe del carrello con quel tipo e prendo per prima le righe a prezzo pieno.
 
-					elargito = true;
+			var righeOma1 = cin.righeCarrello
+				.Where( r => r.prodotto.Equals( _promoProdXProd.prodottoElargito ) && r.sconto == null );
+			var righeOma2 = cin.righeCarrello
+				.Where( r => r.prodotto.Equals( _promoProdXProd.prodottoElargito ) && r.sconto != null ).OrderByDescending( r => r.sconto );
+
+			// Faccio due giri : 1) righe non ancora scontate. 2) righe scontate
+			for( int ii=1; ii<2; ii++ ) {
+				var righeOma = (ii == 1 ? righeOma1 : righeOma2);
+				foreach( RigaCarrello riga in righeOma ) {
+					if( qtaElarg >= riga.quantita ) {
+						riga.sconto = riga.prezzoLordoUnitario;
+						qtaElarg -= riga.quantita;
+						elargito = true;
+					}
 				}
 			}
 
